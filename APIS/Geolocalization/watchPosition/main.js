50fa310e6f46f6c68f17fc7a3d15fdd7665bc891
Diego Ruffa <92127502+DiezRu33@users.noreply.github.com>
2024-04-01 02:40:20 -0300

    Update main.js
    edito

--- APIS/Geolocalization/watchPosition/main.js
+++ APIS/Geolocalization/watchPosition/main.js
@@ -2,28 +2,16 @@ var id, target, options;
 
 function success(pos){
 	var crd = pos.coords;
-
-
-if(target.latitude === crd.latitude && target.longitud === crd.longitude){
-	console.log("Congratulation, you reach the target");
-	navigator.geolocation.clearWatch(id);
-}
+	if(target.latitude === crd.latitude && target.longitud === crd.longitude){
+		console.log("Congratulation, you reach the target");
+		navigator.geolocation.clearWatch(id)
+	} 
 }
 
-function error(err){
-	console.warn("ERROR(" + err.code + "): " + err.message);
-};
+function error( err ){ console.warn( "ERROR(" + err.code + "): " + err.message )}
 
-target = {
-	latitude : 0,
-	longitude: 0,
-}
+target = { latitude : 0, longitude: 0 }
 
-options = {
-	enableHighAccuracy: false,
-	timeout: 5000,
-	maximumAge: 0
-}
+options = { enableHighAccuracy: false, timeout: 5000, maximumAge: 0 }
 
-id = 
-navigator.geolocation.watchPosition(success, error, options)
+id = navigator.geolocation.watchPosition(success, error, options)
