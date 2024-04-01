9b62e9df7170b6cdc22f1bacca3c08014a60a637
Diego Ruffa <92127502+DiezRu33@users.noreply.github.com>
2024-04-01 02:47:51 -0300

    Update tasks.js
    edito

--- AsyncAwait-Promises-Callbacks/tasks.js
+++ AsyncAwait-Promises-Callbacks/tasks.js
@@ -3,12 +3,12 @@ const sleep = util.promisify(setTimeout);
 
 module.exports = {
 	async taskOne() {
-		await sleep(4000)//mls;
-		return 'ONE VALUE';
+		await sleep(4000) //mls
+		return '... 4, 5, 6...';
 	},
 
 	async taskTwo(){
 		await sleep(2000);
-		return 'TWO VALUE'
+		return '... 2, 3, 4...';
 	}
 };
